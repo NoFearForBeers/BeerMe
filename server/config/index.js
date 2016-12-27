e0d8568e95cbbd1f6d04e1b3852186f03eb25fd5
nadamova <aracna2k@yahoo.com>
2016-12-27 19:07:02 +0200

    fixes

--- server/config/index.js
+++ server/config/index.js
@@ -1,3 +1,5 @@
+/* globals module */
+
 let port = process.env.PORT || 3000;
 let connection = process.env.MONGODB_URI || "mongodb://localhost/beerme";
 let url = process.env.NODE_ENV || "http://localhost:3000";
