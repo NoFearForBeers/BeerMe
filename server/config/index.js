a9fc728e48f1f40b568a9a317768a76ac8f71173
nadamova <aracna2k@yahoo.com>
2016-12-27 22:49:58 +0200

    connect.string opit 1

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,6 @@
 /* globals module */
 
-let port = process.env.PORT || 3000;
+/*let port = process.env.PORT || 3000;
 let connection = process.env.MONGODB_URI || "mongodb://localhost/beerme";
 let url = process.env.NODE_ENV || "http://localhost:3000";
 
@@ -8,4 +8,12 @@ module.exports = {
     rootUrl: url,
     connectionString: connection,
     port: port
-};
+};*/
+
+let port = process.env.PORT || 3000;
+  		  
+  module.exports = {
+ // connectionString: "mongodb://localhost/beerme",
+connectionString: "mongodb://heroku_sff40t7w:ueu3fo3si6q62tb4q0ko45gvjr@ds141328.mlab.com:41328/heroku_sff40t7w",
+port: port
+  }; 
