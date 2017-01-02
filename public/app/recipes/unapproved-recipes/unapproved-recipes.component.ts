1967dfd7c5b0afbcaa8b6a50a1e51cb6183a7d5d
PBoyanov <pepiboyanovbg@gmail.bg>
2017-01-02 16:37:55 +0200

    Unapproved recipes - security update

--- public/app/recipes/unapproved-recipes/unapproved-recipes.component.ts
+++ public/app/recipes/unapproved-recipes/unapproved-recipes.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { PageComponent } from '../../shared/page.component';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../services/recipe.service';
+import { AuthService } from '../../authentication/services/auth.service';
 
 @Component({
   moduleId: module.id,
@@ -15,7 +16,7 @@ import { RecipeService } from '../services/recipe.service';
 export class UnapprovedRecipesComponent implements PageComponent {
   recipeList: Recipe[] = [];
 
-  constructor(private recipeService: RecipeService) {
+  constructor(private recipeService: RecipeService, private authService: AuthService) {
   }
 
   ngOnInit() {
